115ce1c914a165cb8de3dddee8c0d232835512a4
SMuyesser <smuyesser@gmail.com>
2017-03-29 16:25:28 -0700

    functions add for wordCount, uniqueWordcount, and avgWordLength

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-function getResults(event) {
+// function to count all words
+function countWords(words) {
+	return words.length;
+}
+
+// function to count words not including repeated words
+function countUniqueWords(words) {
+	var uniqueWords = [];
+	for (var i = 0; i < words.length; i++) {
+		if (uniqueWords.indexOf(words[i]) === -1) {
+			uniqueWords.push(words[i])
+		}
+	}
+	return uniqueWords.length;
+}
+
+// function to determine average word length
+function getAverageWordLength(words) {
+	var total = words.join('').length;
+	return total / countWords(words);
+}
+
+
+
+function results(event) {
 	$('.submit').click(function showResults(event) {
 		$('dl.text-report').removeClass('hidden');
 	});
 }
-
-getResults();
+results();
